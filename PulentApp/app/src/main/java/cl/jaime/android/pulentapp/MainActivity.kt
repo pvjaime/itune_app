e68d5fa41124a53a455974f91f7f120c6018e5b9
Jaime Perez <jaime.perez.varas@gmail.com>
2020-05-22 02:51:39 -0400

    Initial Commit

---
+++ PulentApp/app/src/main/java/cl/jaime/android/pulentapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package cl.jaime.android.pulentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
